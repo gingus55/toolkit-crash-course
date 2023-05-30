4e397612c2f6d2d3c859dd99631c2bbe02af152f
gingus55 <gingus55@hotmail.com>
2023-05-30 13:38:18 +0100

    button func

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import ReservationCard from "./components/ReservationCard";
+import { addReservation } from "./features/reservationSlice";
 
 function App() {
+  const [reservationNameInput, setReservationNameInput] = useState("");
+  const reservations = useSelector((state) => state.reservations.value);
+
+  const dispatch = useDispatch();
+
+  const handleAddReservations = () => {
+    if (!reservationNameInput) return;
+    dispatch(addReservation);
+    setReservationNameInput("");
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="reservation-container">
           <div>
             <h5 className="reservation-header">Reservations</h5>
-            <div className="reservation-cards-container">
-              <div className="reservation-card-container">Laith Harb</div>
-            </div>
+            <div className="reservation-cards-container"></div>
+            {reservations.map((name) => {
+              return <ReservationCard name={name} />;
+            })}
           </div>
           <div className="reservation-input-container">
             <input />
@@ -23,8 +38,11 @@ function App() {
             <div className="customer-foods-container">
               <div className="customer-food"></div>
               <div className="customer-food-input-container">
-                <input />
-                <button>Add</button>
+                <input
+                  value={reservationNameInput}
+                  onChange={(e) => setReservationNameInput(e.target.value)}
+                />
+                <button onClick={handleAddReservations}>Add</button>
               </div>
             </div>
           </div>
---
+++ src/components/ReservationCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReservationCard(name) {
+  return <div className="reservation-card-container">{name}</div>;
+}
